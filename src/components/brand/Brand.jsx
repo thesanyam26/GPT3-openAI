24eaf6efa4c824f11b508a0e9ed2f1d263c0d39a
thesanyam26 <sm26modi@gmail.com>
2023-02-23 17:26:03 +0530

    brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { google, slack, Bitmap, dropbox, shopify } from './import';
 import './brand.css';
 
-function Brand() {
-  return (
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
     <div>
-        Brand
+      <img src={google} />
     </div>
-  )
-}
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={Bitmap} slt='Bitmap' />
+    </div>
+    {/* <div>
+      <img src={atlassian} />
+    </div> */}
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
+);
 
-export default Brand
+export default Brand;
---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import dropbox from '../../assets/dropbox.png'
+import Bitmap from '../../assets/Bitmap.png'
+import shopify from '../../assets/shopify.png'
+
+export {
+    google,
+    slack,
+    dropbox,
+    Bitmap,
+    shopify
+}
