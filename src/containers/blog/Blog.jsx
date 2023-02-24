e1eebdb43e93b362509e986a9d39e18aeb2d2757
thesanyam26 <sm26modi@gmail.com>
2023-02-24 23:14:02 +0530

    change

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => (
     </div>
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA">
-        <Article imgUrl={blogA} date="Feb 24, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blogA} date="Feb 24, 2023" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
       </div>
       <div className="gpt3__blog-container_groupB">
         <Article imgUrl={blog02} date="Feb 24, 2023" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
